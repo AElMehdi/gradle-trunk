5a6a9d9e696e36cfc71adec9777e60d03a1cd53f
AElMehdi <assirelmehdi@gmail.com>
2019-10-26 17:50:03 +0200

    Add npm intsall task

--- java-spa/frontend/build.gradle.kts
+++ java-spa/frontend/build.gradle.kts
@@ -11,15 +11,21 @@ repositories {
 }
 
 node {
-  version = "10.15.0"
+  version = "10.15.0" // Bug: It constructs bin node file name based on the system arch info (e.g x86.tar.gz) which is not found in the dist server
   download = false
 }
 
-task<NpmTask>("ng-build"){
+task<NpmTask>("npm-install"){
   inputs.dir("src")
   inputs.file("package.json")
   inputs.file("package-lock.json")
 
-  setArgs(listOf("run-script", "build", "--", "--output-path=build"))
+  setArgs(listOf("install"))
 }
+task<NpmTask>("ng-build") {
+  dependsOn("npm-install")
+
+  setArgs(listOf("run-script", "build", "--output-path=build"))
+}
+
 tasks.getByName("assemble").dependsOn("ng-build")
